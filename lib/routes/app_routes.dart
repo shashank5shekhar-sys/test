2294864f1f2a4c5bfbb0109c536409de7a9650e0
shashank5shekhar-sys <shashank5.shekhar@paytmservices.com>
2026-04-24 13:10:13 +0530

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,65 +1,8 @@
-// // lib/routes/app_routes.dart
-// import 'package:flutter/material.dart';
-// import '../screens/splash_screen.dart';
-// import '../screens/login_screen.dart';
-// import '../screens/signup_screen.dart';
-// import '../screens/home_screen.dart';
-// import '../screens/profile_screen.dart';
-// import '../screens/quiz_list_screen.dart';
-// import '../screens/quiz_screen.dart';
-// import '../screens/result_screen.dart';
-
-// class AppRoutes {
-//   static const String splash    = '/';
-//   static const String login     = '/login';
-//   static const String signup    = '/signup';
-//   static const String home      = '/home';
-//   static const String profile   = '/profile';
-//   static const String quizList  = '/quiz-list';
-//   static const String quiz      = '/quiz';
-//   static const String result    = '/result';
-
-//   static Map<String, WidgetBuilder> get routes => {
-//     splash:   (_) => const SplashScreen(),
-//     login:    (_) => const LoginScreen(),
-//     signup:   (_) => const SignupScreen(),
-//     home:     (_) => const HomeScreen(),
-//     profile:  (_) => const ProfileScreen(),
-//     quizList: (_) => const QuizListScreen(),
-//   };
-
-//   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
-//     switch (settings.name) {
-//       case quiz:
-//         final args = settings.arguments as Map<String, dynamic>;
-//         return _fadeRoute(QuizScreen(
-//           quizId: args['quizId'],
-//           quizTitle: args['quizTitle'],
-//         ));
-//       case result:
-//         final args = settings.arguments as Map<String, dynamic>;
-//         return _fadeRoute(ResultScreen(
-//           score: args['score'],
-//           total: args['total'],
-//           quizTitle: args['quizTitle'],
-//         ));
-//       default:
-//         return null;
-//     }
-//   }
-
-//   static PageRouteBuilder _fadeRoute(Widget page) {
-//     return PageRouteBuilder(
-//       pageBuilder: (_, __, ___) => page,
-//       transitionsBuilder: (_, animation, __, child) {
-//         return FadeTransition(opacity: animation, child: child);
-//       },
-//       transitionDuration: const Duration(milliseconds: 300),
-//     );
-//   }
-// }
+// lib/routes/app_routes.dart
 import 'package:flutter/material.dart';
 import '../screens/splash_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/signup_screen.dart';
 import '../screens/home_screen.dart';
 import '../screens/profile_screen.dart';
 import '../screens/quiz_list_screen.dart';
@@ -68,6 +11,8 @@ import '../screens/result_screen.dart';
 
 class AppRoutes {
   static const String splash    = '/';
+  static const String login     = '/login';
+  static const String signup    = '/signup';
   static const String home      = '/home';
   static const String profile   = '/profile';
   static const String quizList  = '/quiz-list';
@@ -76,6 +21,8 @@ class AppRoutes {
 
   static Map<String, WidgetBuilder> get routes => {
     splash:   (_) => const SplashScreen(),
+    login:    (_) => const LoginScreen(),
+    signup:   (_) => const SignupScreen(),
     home:     (_) => const HomeScreen(),
     profile:  (_) => const ProfileScreen(),
     quizList: (_) => const QuizListScreen(),
@@ -85,24 +32,20 @@ class AppRoutes {
     switch (settings.name) {
       case quiz:
         final args = settings.arguments as Map<String, dynamic>;
-        return _fadeRoute(
-          QuizScreen(
-            quizId: args['quizId'],
-            quizTitle: args['quizTitle'],
-          ),
-        );
-
+        return _fadeRoute(QuizScreen(
+          quizId: args['quizId'],
+          quizTitle: args['quizTitle'],
+        ));
       case result:
         final args = settings.arguments as Map<String, dynamic>;
-        return _fadeRoute(
-          ResultScreen(
-            score: args['score'],
-            total: args['total'],
-            quizTitle: args['quizTitle'],
-          ),
-        );
+        return _fadeRoute(ResultScreen(
+          score: args['score'],
+          total: args['total'],
+          quizTitle: args['quizTitle'],
+        ));
+      default:
+        return null;
     }
-    return null;
   }
 
   static PageRouteBuilder _fadeRoute(Widget page) {
@@ -115,3 +58,4 @@ class AppRoutes {
     );
   }
 }
+
