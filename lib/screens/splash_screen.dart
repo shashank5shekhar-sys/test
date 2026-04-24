251460c609bacc6814936d4b053d5268cbedeb1c
shashank5shekhar-sys <shashank5.shekhar@paytmservices.com>
2026-04-24 13:14:49 +0530

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -27,7 +27,6 @@ class _SplashScreenState extends State<SplashScreen> {
     } else {
       Navigator.pushReplacementNamed(context, AppRoutes.login);
     }
-   
   }
 
   @override
@@ -114,4 +113,3 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
-lib/screens/splash_screen.dart
