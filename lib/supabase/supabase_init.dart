27fccbe23ef6baf1981d4cee06357add139a03bd
shashank5shekhar-sys <shashank5.shekhar@paytmservices.com>
2026-04-24 12:52:16 +0530

    Update supabase_init.dart

--- lib/supabase/supabase_init.dart
+++ lib/supabase/supabase_init.dart
@@ -1,4 +1,17 @@
-// lib/supabase/supabase_init.dart
+// // lib/supabase/supabase_init.dart
+// import 'package:supabase_flutter/supabase_flutter.dart';
+// import '../utils/constants.dart';
+
+// class SupabaseInit {
+//   static Future<void> initialize() async {
+//     await Supabase.initialize(
+//       url: AppConstants.supabaseUrl,
+//       anonKey: AppConstants.supabaseAnonKey,
+//     );
+//   }
+
+//   static SupabaseClient get client => Supabase.instance.client;
+// }
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../utils/constants.dart';
 
@@ -7,6 +20,12 @@ class SupabaseInit {
     await Supabase.initialize(
       url: AppConstants.supabaseUrl,
       anonKey: AppConstants.supabaseAnonKey,
+
+      // 🔥 THIS FIXES YOUR ERROR
+      authOptions: const FlutterAuthClientOptions(
+        autoRefreshToken: false,
+        persistSession: false,
+      ),
     );
   }
 
